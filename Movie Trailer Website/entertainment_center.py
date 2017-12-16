b701277c3d64a44c850dc5c9890b212bc3b0b851
cherry86222 <740803860@qq.com>
2017-12-16 16:34:14 +1300

    change according to review
    1. add comments
    2. add steps in README.md
    3. change some format in py

--- Movie Trailer Website/entertainment_center.py
+++ Movie Trailer Website/entertainment_center.py
@@ -1,31 +1,28 @@
 import fresh_tomatoes
 import media
 
-Thor = media.Movie("Thor",
-	"A marvel story of thor",
-	"https://fsmedia.imgix.net/99/13/42/3b/5ccf/4e04/aa4d/7b21323cd5f0/chris-hemsworth-as-thor.jpeg",
-	"https://www.youtube.com/watch?v=ue80QwXMRHg")
+"""This is to define three instances from class Movie in file media. It passes the 
+defined parameter in to the class and initiate the instances"""
+Thor = media.Movie("Thor", 
+    "A marvel story of thor", 
+    "https://fsmedia.imgix.net/99/13/42/3b/5ccf/4e04/aa4d/7b21323cd5f0/chris-hemsworth-as-thor.jpeg", 
+    "https://www.youtube.com/watch?v=ue80QwXMRHg")
 
-#print(toy_story.storyline)
 
 panda = media.Movie("Kung Fu Panda",
 	"A panda 's funny story",
 	"http://www.gstatic.com/tv/thumb/movieposters/175618/p175618_p_v8_ad.jpg",
 	"https://www.youtube.com/watch?v=C1bRkCenm20")
-#print(avatar.storyline)
 
-#avatar.show_trailer()
 
 sunrise = media.Movie("Love before sunrise",
 	"A story about love during the trip",
 	"http://i.imgur.com/tocI1Ep.jpg",
 	"https://www.youtube.com/watch?v=25v7N34d5HE")
 
+"""This is to define the list of movies to store three instances of Movie class"""
+movies = [Thor, panda, sunrise]
 
-
-movies = [Thor,panda,sunrise]
+"""This is to open the movie page from the method open_movies_page in fresh_tomatoes
+and pass the parameter of list movies containing three instances"""
 fresh_tomatoes.open_movies_page(movies)
-
-#print(media.Movie.__doc__)
-#print(media.Movie.__name__)
-#print(media.Movie.__module__)
